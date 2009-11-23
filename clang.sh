f0906b45a28a0f8f7b96373debc11afeb97e0912
Cyril Godefroy <cyril@MBA.local>
2009-11-23 15:27:19 +0100

    Test

--- clang.sh
+++ clang.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-~/checker/scan-build --status-bugs -k -V \
-	-warn-objc-missing-dealloc \
-	xcodebuild -sdk iphonesimulator2.2.1 -configuration Debug	
+~/checker/scan-build --status-bugs -k -V  -warn-objc-missing-dealloc  xcodebuild -configuration Debug	
+
+
+#~/checker/scan-build --status-bugs -k -V  -warn-objc-missing-dealloc  xcodebuild -sdk iphonesimulator2.2.1 -configuration Debug	
 
 
 #scan-build -k -V xcodebuild
